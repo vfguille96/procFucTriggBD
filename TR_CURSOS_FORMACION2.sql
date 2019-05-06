9b56f0b1577a0b6046966886be35595ec64ad706
Guillermo Vera <vfguille@gmail.com>
2019-05-06 13:46:55 +0200

    Trigger cf

--- TR_CURSOS_FORMACION2.sql
+++ TR_CURSOS_FORMACION2.sql
@@ -33,28 +33,71 @@ begin
 	end if;
 end//
 
-drop trigger if exists bi_edicion//
-create trigger bi_edicion before insert on edicion for each row
+drop function if exists cumpleRequisitios//
+create function cumpleRequisitios(fechaReq date, emp char(5), cur char(15)) returns tinyint(1) deterministic
 begin
-	if not exists (select * from empleado where capacitado = 'S' and new.profesor=codEmp) then
-		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El profesor asignado no está capacitado';			
-	end if;
-	
-/*	if exists (select * from matricula where new.profesor = empleado and new.curso = codCurso and new.fecha = fecha) then
+	declare salida tinyint(1) default 0;
+	declare cursoRequerido char(15);
+
+	declare cursorMatricula cursor for (select cursoReq from prerreq where curso = cur and obligatorio);
+	declare continue handler for not found set salida = 1;
+
+	open cursorMatricula;
+
+	while (not salida) do
+		fetch cursorMatricula into cursoRequerido;
+
+		if (salida) then
+			close cursorMatricula;
+			return 1;
+		end if;
+
+		if not exists (select * from matricula where curso = cursoRequerido and fecha < fechaReq and empleado = emp) then
+			close cursorMatricula;
+			return 0;
+		end if;
+
+	end while;
+end//
+
+drop trigger if exists bi_matricula//
+create trigger bi_matricula before insert on matricula for each row
+begin
+	if exists (select * from edicion where curso = new.curso and fecha = new.fecha and profesor = new.empleado) then
 		SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'El profesor no puede ser alumno a la vez.';
+	end if;
+
+	if exists (select * from prerreq where prerreq.curso = new.curso and obligatorio) then
+		if not cumpleRequisitios(new.fecha, new.empleado, new.curso) then
+			signal sqlstate '45000' set mysql_errno = 5006, message_text = 'El empleado no cumple los requisitos.';
+		end if;
+	end if;
+end//
+
+drop trigger if exists bu_matricula//
+create trigger bu_matricula before update on matricula for each row
+begin
+
+-- No se puede modificar nada de matrícula.
+signal sqlstate '45000' set mysql_errno = 5007, message_text = 'No se puede modificar nada de matrícula.';
+
+/*	if exists (select * from edicion where curso = new.curso and fecha = new.fecha and profesor = new.empleado) then
+		SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'El profesor no puede ser alumno a la vez.';
+	end if;
+
+	if exists (select * from prerreq where prerreq.curso = new.curso and obligatorio) then
+		if not cumpleRequisitios(new.fecha, new.empleado, new.curso) then
+			signal sqlstate '45000' set mysql_errno = 5006, message_text = 'El empleado no cumple los requisitos.';
+		end if;
 	end if;*/
 end//
 
-/*drop trigger if exists bi_matricula//
-create trigger bi_matricula before insert on matricula for each row
+drop trigger if exists ad_matricula//
+create trigger ad_matricula before delete on matricula for each row
+sql security invoker
 begin
-	declare error_clave_primaria condition for SQLSTATE '21000';
-	declare cursoRequerido varchar(15);
-	if (select new.curso in (select curso from prerreq p1 where obligatorio and cursoReq not in (select curso from matricula where empleado = new.empleado))) then
-		set foreign_key_checks = 0;
-		set cursoRequerido = (select curso from prerreq p1 where obligatorio and cursoReq not in (select curso from matricula where empleado = new.empleado));
-		SIGNAL error_clave_primaria SET MESSAGE_TEXT = 'El curso a insertar requiere otro curso';
-	end if;
-end//*/
+	insert into matricula values (current_timestamp(), current_user(), old.fecha, old.curso, old.empleado);
+
+end//
 
 delimiter ;
